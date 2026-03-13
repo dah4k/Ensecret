c7f5726b537525a10b256efd96630e3bcc2f3513
dah4k <84741290+dah4k@users.noreply.github.com>
2026-03-12 21:07:53 -0400

    [CMD] Call F# Scriptlet.Hello() from C# program

---
+++ Cmd/Program.cs
@@ -0,0 +1,14 @@
+﻿// Copyright 2026 dah4k
+// SPDX-License-Identifier: EPL-2.0
+
+namespace Ensecret.Cmd;
+
+using static Ensecret.Scriptlet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Hello("World!");
+    }
+}
