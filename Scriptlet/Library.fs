084e9fff632d0706a71a453726441ec9f9b17f74
dah4k <84741290+dah4k@users.noreply.github.com>
2026-03-13 00:00:08 -0400

    [SCRIPTLET] Add UploadFile() and other functions

--- Scriptlet/Library.fs
+++ Scriptlet/Library.fs
@@ -11,6 +11,7 @@ module Scriptlet =
 
     let Hello (name: string) = printfn "Hello %s" name
 
+
     let LoadEmbeddedFile (pathname: string) : string =
         let assembly = Assembly.GetCallingAssembly()
         let name = assembly.GetName().Name
@@ -20,3 +21,34 @@ module Scriptlet =
 
         use streamReader = new StreamReader(stream, Encoding.UTF8)
         streamReader.ReadToEnd()
+
+
+    let UploadFile (localPath: string, remotePath: string) : string =
+        $$"""
+        sudo mkdir --parents ${dirname {{remotePath}}}
+
+        cat <<'END_OF_HEREDOC_UPLOAD_FILE' | sudo tee {{remotePath}}
+        {{LoadEmbeddedFile(localPath)}}
+        END_OF_HEREDOC_UPLOAD_FILE
+        """
+
+
+    let UploadFileWithChangeMode (localPath: string, remotePath: string, mode: string) : string =
+        $$"""
+        {{UploadFile(localPath, remotePath)}}
+
+        sudo chmod {{mode}} {{remotePath}}
+        """
+
+
+    let InstallSystemdService (localServicePath: string) : string =
+        let serviceName = Path.GetFileName(localServicePath)
+        let remoteServicePath = $"/etc/systemd/system/{serviceName}"
+
+        $$"""
+        {{UploadFile(localServicePath, remoteServicePath)}}
+
+        sudo systemctl daemon-reload
+        sudo systemctl enable {{serviceName}}
+        sudo systemctl start {{serviceName}}
+        """
