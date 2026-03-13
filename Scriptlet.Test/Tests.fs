1cb2c99873a14918528b7a26f93f0a5c2d5d7a0f
dah4k <84741290+dah4k@users.noreply.github.com>
2026-03-12 21:06:11 -0400

    Initial commit

---
+++ Scriptlet.Test/Tests.fs
@@ -0,0 +1,12 @@
+﻿// Copyright 2026 dah4k
+// SPDX-License-Identifier: EPL-2.0
+
+module Tests
+
+open Ensecret.Scriptlet
+open Xunit
+
+[<Fact>]
+let ``Say Hello`` () =
+    Hello "World!"
+    Assert.True(true)
---
+++ Scriptlet/Library.fs
@@ -0,0 +1,8 @@
+﻿// Copyright 2026 dah4k
+// SPDX-License-Identifier: EPL-2.0
+
+module Ensecret.Scriptlet
+
+let Hello (name: string) =
+    printfn "Hello %s" name
+
