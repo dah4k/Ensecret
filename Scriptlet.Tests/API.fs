a2ace581d43674f131e40d8df50dab1cff88c1a4
dah4k <84741290+dah4k@users.noreply.github.com>
2026-03-22 20:27:26 -0400

    [TEST] Reorg and add more specific Scriptlet.deindent test cases

--- Scriptlet.Tests/API.fs
+++ Scriptlet.Tests/API.fs
@@ -1,39 +1,47 @@
 ﻿// Copyright 2026 dah4k
 // SPDX-License-Identifier: EPL-2.0
 
-module Tests
+module Tests.API
 
 open Ensecret.Scriptlet
 open Xunit
 
+//==============================================================================
+//  Scriptlet public API test cases
+//==============================================================================
+
 [<Fact>]
-let ``Say Hello`` () =
+let ``TC.1.000 - Say Hello`` () =
     Hello "World!"
     Assert.True(true)
 
 [<Fact>]
-let ``Embedded resource file is not empty`` () =
+let ``TC.1.100 - Embedded resource file is not empty`` () =
     Assert.NotEmpty(LoadEmbeddedFile("Files/greetings.txt"))
 
 [<Fact>]
-let ``Embedded resource file is from us`` () =
+let ``TC.1.110 - Embedded resource file is from us`` () =
     Assert.Matches("^from Scriptlet.Tests", LoadEmbeddedFile("Files/greetings.txt"))
 
 [<Fact>]
-let ``UploadFile result contains HEREDOC`` () =
+let ``TC.1.200 - UploadFile result contains HEREDOC`` () =
     Assert.Matches("HEREDOC", UploadFile("Files/greetings.txt", "/tmp/test.txt"))
 
 [<Fact>]
-let ``UploadFileWithChangeMode result contains chmod`` () =
+let ``TC.1.210 - UploadFileWithChangeMode result contains HEREDOC`` () =
+    Assert.Matches("HEREDOC", UploadFileWithChangeMode("Files/greetings.txt", "/tmp/test.txt", "640"))
+
+[<Fact>]
+let ``TC.1.211 - UploadFileWithChangeMode result contains chmod`` () =
     Assert.Matches("chmod", UploadFileWithChangeMode("Files/greetings.txt", "/tmp/test.txt", "640"))
 
 [<Fact>]
-let ``InstallSystemdService result contains daemon-reload`` () =
+let ``TC.1.300 - InstallSystemdService result contains daemon-reload`` () =
     Assert.Matches("daemon-reload", InstallSystemdService("Files/greetings.txt"))
 
 [<Fact>]
 // Reference: https://github.com/dotnet/fsharp/issues/13995
-let ``Indentation aware raw string literals (similar to C# 11) #13995`` () =
+let ``TC.1.400 - Indentation aware raw string literals (similar to C# 11) #13995`` () =
     let htmlString1 =
         """<!DOCTYPE html>
 <html lang="en">
@@ -55,5 +63,6 @@ let ``Indentation aware raw string literals (similar to C# 11) #13995`` () =
              </body>
         </html>
         """
+        |> deindent
 
     Assert.Equal(htmlString1, htmlString2)
---
+++ Scriptlet.Tests/deindent.fs
@@ -0,0 +1,97 @@
+﻿// Copyright 2026 dah4k
+// SPDX-License-Identifier: EPL-2.0
+
+module Tests.deindent
+
+open Ensecret.Scriptlet
+open Xunit
+
+//==============================================================================
+//  More specific Scriptlet.deindent test cases
+//==============================================================================
+
+[<Fact>]
+let ``TC.0.001 - Empty string remains empty string`` () =
+    let expected = ""
+    let actual = "" |> deindent
+
+    Assert.Equal(expected, actual)
+
+
+[<Fact>]
+let ``TC.0.100 - No-indent zero line block becomes empty string`` () =
+    let expected = ""
+
+    let actual =
+        """
+"""
+        |> deindent
+
+    Assert.Equal(expected, actual)
+
+
+[<Fact>]
+let ``TC.0.101 - No-indent one empty line block becomes empty string`` () =
+    let expected = ""
+
+    let actual =
+        """
+
+"""
+        |> deindent
+
+    Assert.Equal(expected, actual)
+
+
+[<Fact>]
+let ``TC.0.102 - No-indent two empty lines block becomes one newline`` () =
+    let expected = "\n"
+
+    let actual =
+        """
+
+
+"""
+        |> deindent
+
+    Assert.Equal(expected, actual)
+
+
+[<Fact>]
+let ``TC.0.200 - Indent-8 zero line block becomes empty string`` () =
+    let expected = ""
+
+    let actual =
+        """
+        """
+        |> deindent
+
+    Assert.Equal(expected, actual)
+
+
+[<Fact>]
+[<Trait("Category", "deindent")>]
+let ``TC.0.201 - Indent-8 one empty line block becomes empty string`` () =
+    let expected = ""
+
+    let actual =
+        """
+
+        """
+        |> deindent
+
+    Assert.Equal(expected, actual)
+
+
+[<Fact>]
+let ``TC.0.202 - Indent-8 two empty lines becomes one newline`` () =
+    let expected = "\n"
+
+    let actual =
+        """
+
+
+        """
+        |> deindent
+
+    Assert.Equal(expected, actual)
