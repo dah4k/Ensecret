2d49ef7f4a775381c846cb94945c2bbbaf7f5d92
dah4k <84741290+dah4k@users.noreply.github.com>
2026-03-28 00:54:06 -0400

    [SCRIPTLET] Add Scriptlet.trimIndent
    TODO: Scriptlet.trimEnclosingNewlines

--- Scriptlet/Library.fs
+++ Scriptlet/Library.fs
@@ -8,12 +8,43 @@ open System.Linq
 open System.IO
 open System.Reflection
 open System.Text
+open System
 
 module Scriptlet =
 
     let Hello (name: string) = printfn "Hello %s" name
 
-    let deindent (textBlock: string) : string = textBlock
+    let INDENTATION_CHARACTER = '\x20' // ASCII space character
+
+    let extractIndentationPrefix (line: string) : string =
+        let n = line.Length
+        let prefix = new String(INDENTATION_CHARACTER, n)
+
+        if line = prefix then prefix else String.Empty
+
+    let trimIndent (prefix: string, line: string) : string =
+        if prefix = String.Empty then
+            line
+        elif line = String.Empty then
+            line
+        elif line.StartsWith(prefix) then
+            line.Remove(0, prefix.Length)
+        else
+            // This should be invalidArg "ERROR: trimIndent on outdented line".
+            // Workaround to recover using String.TrimStart() for now...
+            line.TrimStart()
+
+    let trimEnclosingNewlines (textBlock: string) : string = textBlock
+
+    let deindent (textBlock: string) : string =
+        let textLines = textBlock.Split '\n'
+        let prefix = textLines |> Seq.last |> extractIndentationPrefix
+
+        textLines
+        |> Seq.map (fun x -> trimIndent (prefix, x))
+        |> Seq.reduce (fun x y -> x + "\n" + y)
+        |> trimEnclosingNewlines
+
 
     // Modified from https://stackoverflow.com/questions/44659856/is-this-the-best-way-to-get-the-calling-assembly-in-windows-10-uwp
     let GetParentAssemblies () =
