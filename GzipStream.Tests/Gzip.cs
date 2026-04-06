95131e3e8a37d41ce71d36d25bf33fd137d9e5e5
dah4k <84741290+dah4k@users.noreply.github.com>
2026-04-06 18:26:31 -0400

    [TEST] Add C# GzipStream.Tests using xUnit.net v3

---
+++ GzipStream.Tests/Gzip.cs
@@ -0,0 +1,33 @@
+// Copyright 2026 dah4k
+// SPDX-License-Identifier: EPL-2.0
+
+namespace GzipStream.Tests;
+
+using System.IO.Compression;
+using System.Text;
+
+public class UnitTest1
+{
+    // Modified from https://khalidabuhakmeh.com/compress-strings-with-dotnet-and-csharp/
+    byte[] GzipText(string plainText)
+    {
+        using (var inStream = new MemoryStream(Encoding.UTF8.GetBytes(plainText)))
+        using (var outStream = new MemoryStream())
+        using (var gzStream = new GZipStream(outStream, CompressionMode.Compress))
+        {
+            inStream.CopyTo(gzStream);
+            gzStream.Close();
+            return outStream.ToArray();
+        }
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // echo -n "abcd" | gzip | base64
+        string expected = "H4sIAAAAAAAAA0tMSk4BABHNgu0EAAAA";
+        string actual = System.Convert.ToBase64String(GzipText("abcd"));
+
+        Assert.Equal(expected, actual);
+    }
+}
