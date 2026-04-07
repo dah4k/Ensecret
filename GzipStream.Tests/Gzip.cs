05d315388528c1e941e0a5c213bdbe33eda600bd
dah4k <84741290+dah4k@users.noreply.github.com>
2026-04-06 22:15:48 -0400

    [TEST] Relabel C# GzipStream.Tests and add new Gzip test cases
    FIXME: C# Gzip on empty byte array returns empty byte array

--- GzipStream.Tests/Gzip.cs
+++ GzipStream.Tests/Gzip.cs
@@ -1,12 +1,12 @@
 // Copyright 2026 dah4k
 // SPDX-License-Identifier: EPL-2.0
 
-namespace GzipStream.Tests;
+namespace Csharp.GzipStream;
 
 using System.IO.Compression;
 using System.Text;
 
-public class UnitTest1
+public class Tests
 {
     // Modified from https://khalidabuhakmeh.com/compress-strings-with-dotnet-and-csharp/
     byte[] GzipText(string plainText)
@@ -21,8 +21,22 @@ public class UnitTest1
         }
     }
 
+    // Modified from https://github.com/dotnet/runtime/issues/122928 (Not planned 2026)
+    // See Also
+    // - https://github.com/dotnet/runtime/issues/112563 (Closed 2025)
+    // - https://github.com/dotnet/runtime/pull/94433 (Merged 2023)
+    byte[] Gzip(byte[] data)
+    {
+        using var outStream = new MemoryStream();
+        using (var gzStream = new GZipStream(outStream, CompressionMode.Compress, true))
+        {
+            gzStream.Write(data, 0, data.Length);
+        }
+        return outStream.ToArray();
+    }
+
     [Fact]
-    public void GzipBase64_abcd_string()
+    public void GzipTextAndBase64_abcd_string()
     {
         // echo -n "abcd" | gzip | base64
         string expected = "H4sIAAAAAAAAA0tMSk4BABHNgu0EAAAA";
@@ -32,12 +46,26 @@ public class UnitTest1
     }
 
     [Fact]
-    public void GzipBase64_empty_string()
+    public void GzipTextAndBase64_empty_string()
     {
         // echo -n "" | gzip | base64
         string expected = "H4sIAAAAAAAAAwMAAAAAAAAAAAA=";
         string actual = System.Convert.ToBase64String(GzipText(""));
 
         Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Gzip_non_empty_array()
+    {
+        byte[] nonEmptyArray = Encoding.UTF8.GetBytes("abcd");
+        Assert.NotEmpty(Gzip(nonEmptyArray));
+    }
+
+    [Fact]
+    public void Gzip_empty_array()
+    {
+        byte[] emptyArray = Array.Empty<byte>();
+        Assert.NotEmpty(Gzip(emptyArray));
     }
 }
